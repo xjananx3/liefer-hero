b455eba2807a2c32d2d071ecb975a951e1ebdc9d
Jan <janan9980@gmail.com>
2024-07-15 16:11:38 +0200

    Added SpeiseZurBestellungHinzufuegen in controller and viws

--- src/LieferHero/Controllers/SpeiseController.cs
+++ src/LieferHero/Controllers/SpeiseController.cs
@@ -1,5 +1,6 @@
 using System.Security.AccessControl;
 using LieferHero.Data;
+using LieferHero.Models;
 using LieferHero.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -27,5 +28,22 @@ public class SpeiseController : Controller
             })
             .ToListAsync();
         return View(speisenVm);
+    }
+
+    public async Task<IActionResult> SpeiseZurBestellungHinzufuegen(int speiseId, int menge)
+    {
+        var speise = await _context.Speisen.FindAsync(speiseId);
+        if (speise != null)
+        {
+            var bestellSpeise = new SpeiseInBestellung()
+            {
+                Speise = speise,
+                Menge = menge
+            };
+            _context.SpeisenInBestellung.Add(bestellSpeise);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToAction("Index");
     }
 }
