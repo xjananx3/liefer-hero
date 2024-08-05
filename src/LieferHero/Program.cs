d1eeb1c61ee1e93fec7e8b7e45bd962ae4ed4f9f
Jan <janan9980@gmail.com>
2024-08-05 23:37:00 +0200

    Dependency Injection

--- src/LieferHero/Program.cs
+++ src/LieferHero/Program.cs
@@ -1,10 +1,16 @@
 using LieferHero.Data;
+using LieferHero.Interfaces;
+using LieferHero.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ISpeiseRepository, SpeiseRepository>();
+builder.Services.AddScoped<ISpeiseInBestellungRepository, SpeiseInBestellungRepository>();
+builder.Services.AddScoped<IBestellungRepository, BestellungRepository>();
+builder.Services.AddScoped<IAufgegebeneBestellungRepository, AufgegebeneBestellungRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
