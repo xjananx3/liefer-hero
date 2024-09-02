55f29393e7223283c49b19ca25320b5b633bdb13
jack <janan9980@gmail.com>
2024-09-02 21:21:03 +0200

    Deleting unnecessary Files

---
+++ src/LieferHero/Migrations/20240716141651_RemoveAndRename.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LieferHero.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAndRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AufgegebeneBesellungen");
+
+            migrationBuilder.DropTable(
+                name: "SpeiseHinzufuegenAnfragen");
+
+            migrationBuilder.CreateTable(
+                name: "AufgegebeneBestellungen",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GesamtKosten = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Nachricht = table.Column<string>(type: "TEXT", nullable: false),
+                    BestellungId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AufgegebeneBestellungen", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AufgegebeneBestellungen_Bestellungen_BestellungId",
+                        column: x => x.BestellungId,
+                        principalTable: "Bestellungen",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AufgegebeneBestellungen_BestellungId",
+                table: "AufgegebeneBestellungen",
+                column: "BestellungId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AufgegebeneBestellungen");
+
+            migrationBuilder.CreateTable(
+                name: "AufgegebeneBesellungen",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GesamtKosten = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Nachricht = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AufgegebeneBesellungen", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpeiseHinzufuegenAnfragen",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Menge = table.Column<int>(type: "INTEGER", nullable: false),
+                    SpeiseId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpeiseHinzufuegenAnfragen", x => x.Id);
+                });
+        }
+    }
+}
---
+++ src/LieferHero/Migrations/20240807143050_PhotoService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LieferHero.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhotoService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Speisen",
+                newName: "Preis");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Beschreibung",
+                table: "Speisen",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Bild",
+                table: "Speisen",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Beschreibung",
+                table: "Speisen");
+
+            migrationBuilder.DropColumn(
+                name: "Bild",
+                table: "Speisen");
+
+            migrationBuilder.RenameColumn(
+                name: "Preis",
+                table: "Speisen",
+                newName: "Price");
+        }
+    }
+}
